59ba111643fb98905c6864409bbeaa0ba087e68a
binh150802 <70745818+binh150802@users.noreply.github.com>
2022-10-03 23:15:10 +0700

    Create Models
    Models for student, teacher and account

---
+++ MyAssignment/src/java/model/Account.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Account {
+    private String user;
+    private String pass;
+    private int teacherID;
+    private int studentID;
+
+    public Account() {
+    }
+
+    public Account(String user, String pass, int teacherID, int studentID) {
+        this.user = user;
+        this.pass = pass;
+        this.teacherID = teacherID;
+        this.studentID = studentID;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(int teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    
+    
+}
